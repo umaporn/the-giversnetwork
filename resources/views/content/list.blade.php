1365b48a28efab54c23cfcae986c3eec6cb05871
Darren Porter <darren_po@dtgsiam.com>
2017-07-04 13:57:13 +0700

    Layout for content list page

--- resources/views/content/list.blade.php
+++ resources/views/content/list.blade.php
@@ -7,12 +7,49 @@
 
 @section('content')
 
+    <div class="row column">
+        <label>Choose category
+            <select>
+                <option value=""></option>
+            </select>
+        </label>
+    </div>
+
     <div class="row">
         <div class="small-9 columns">
-            MAIN
+            <div class="content-list">
+                <div class="row small-up-1 medium-up-2 large-up-3">
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="{{ asset('/images/list-item.png') }}" alt="">
+                            </div>
+                            <div class="card-divider">
+                                This is a title
+                            </div>
+                            <div class="card-section">
+                                <div>Jan, 19th 2017 - Category</div>
+                                <p><strong>This is a short description about the post.</strong></p>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="small-3 columns">
-            SIDEBAR
+            <div class="sidebar">
+                <div class="widget">
+                    <a href="#">Archive</a>
+                    <hr>
+                    <div>Jan, 19th 2017 - Category</div>
+                    <p><strong>This is an event post title and needs some
+                               level of truncation, say about 30 char.</strong>
+                    </p>
+                    <a href="#">Read more</a>
+                    <hr>
+                </div>
+            </div>
         </div>
     </div>
 
