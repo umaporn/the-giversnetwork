63d94091ddcd758dc7679cc23f24c18159719f27
EaRNG <e_a_r_n_g@hotmail.com>
2019-05-14 09:54:09 +0700

    Add menu

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Menu
+    |--------------------------------------------------------------------------
+    */
+
+    'top_menu' => [
+        'home' => 'Home',
+        'learn' => 'Learn',
+        'share' => 'Share',
+        'give' => 'Give',
+        'events' => 'Events',
+        'news' => 'News',
+        'organization' => 'Organization',
+    ],
+    'profile_menu' => [
+        'sign_up' => 'Sign up',
+        'log_in' => 'Log in',
+    ],
+
+];
