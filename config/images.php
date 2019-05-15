f5d3cf1774542c9fc3f9addd6afb7016dce06ca1
EaRNG <e_a_r_n_g@hotmail.com>
2019-05-15 11:47:56 +0700

    create news

--- config/images.php
+++ config/images.php
@@ -11,6 +11,16 @@ return [
         'images/herobanner/banner-1.png',
         'images/herobanner/banner-1.png',
     ],
+
+    
+
+    'home' => [
+        'news' => [
+            'home_news_01'             => 'images/home/news-1.png',
+            'home_news_02'             => 'images/home/news-1.png',
+            'home_news_03'             => 'images/home/news-1.png',
+        ],
+    ],
     
 
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,4 +21,38 @@
       <!-- Add Pagination -->
       <div class="swiper-pagination"></div>
    </div>
+   <section class="news padding-content">
+        <div class="grid-x align-middle">
+            <h2 class="cell auto topic-dark">News</h2>
+            <div class="cell shrink view-all">
+                <a href="#">
+                    <span>View All</span>
+                    <i class="fas fa-caret-right"></i> <i class="fas fa-caret-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="grid-x grid-margin-x ">
+                <article class="cell small-12 medium-4">
+                    <figure>
+                        <img src="{{ asset(config('images.home.news.home_news_01' )) }}" class="gallery_img">
+                    </figure>
+                    <h3>Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse</h3>
+                    <time datetime="2019-04-29"><i class="far fa-calendar-alt"></i>29 April 2019</time>
+                </article>
+                <article class="cell small-12 medium-4">
+                    <figure>
+                        <img src="{{ asset(config('images.home.news.home_news_01' )) }}" class="gallery_img">
+                    </figure>
+                    <h3>Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse</h3>
+                    <time datetime="2019-04-29"><i class="far fa-calendar-alt"></i>29 April 2019</time>
+                </article>
+                <article class="cell small-12 medium-4">
+                    <figure>
+                        <img src="{{ asset(config('images.home.news.home_news_01' )) }}" class="gallery_img">
+                    </figure>
+                    <h3>Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse</h3>
+                    <time datetime="2019-04-29"><i class="far fa-calendar-alt"></i>29 April 2019</time>
+                </article>
+        </div>
+   </section>
 @endsection
