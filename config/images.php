47b9a922ac1fb79a3bf1672e9dbb548c0a27f31b
EaRNG <e_a_r_n_g@hotmail.com>
2019-05-15 16:22:30 +0700

    Create share section

--- config/images.php
+++ config/images.php
@@ -20,6 +20,15 @@ return [
             'home_news_02'             => 'images/home/news-1.png',
             'home_news_03'             => 'images/home/news-1.png',
         ],
+        'share' => [
+            'home_share_01'             => 'images/home/share-1.png',
+            'home_share_02'             => 'images/home/share-1.png',
+            'home_share_03'             => 'images/home/share-1.png',
+        ],
+        'profile' => [
+            'user_profile'             => 'images/home/user-profile.jpg',
+            
+        ],
     ],
     
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,4 +55,157 @@
                 </article>
         </div>
    </section>
+   <section class="share">
+        <div class="grid-x align-middle topic padding-content">
+            <div class="cell auto">
+                <i class="fas fa-shapes"></i> <h2 class="topic-light">share</h2> 
+                <span>- Short description to explain share section : Definition</span>
+            </div>
+            <div class="cell shrink view-all">
+                <a href="#">
+                    <span>View All</span>
+                    <i class="fas fa-caret-right"></i> <i class="fas fa-caret-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="grid-x content padding-content">
+            <div class="cell">
+                <ul class="tabs" data-active-collapse="true" data-tabs id="collapsing-tabs">
+                    <li class="tabs-title is-active"><a href="#panel1c" aria-selected="true">Recent Share</a></li>
+                    <li class="tabs-title"><a href="#panel2c">Challenge</a></li>
+                </ul>
+            </div>
+            <div class="cell">
+                <div class="tabs-content" data-tabs-content="collapsing-tabs">
+                    <div class="tabs-panel is-active" id="panel1c">
+                        <div class="grid-x grid-margin-x recent-share">
+                            <article class="cell grid-x medium-12 large-6">
+                                <div class="cell small-12 medium-5">
+                                    <figure>
+                                        <img src="{{ asset(config('images.home.share.home_share_01' )) }}">
+                                    </figure>
+                                </div>
+                                <div class="cell grid-x small-12 medium-7">
+                                    <h4 class="cell">Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget nibh arcu.</h4>
+                                    <div class="cell grid-x align-self-bottom">
+                                        <div class="cell auto profile">
+                                            <figure class="display-profile">
+                                                <img src="{{ asset(config('images.home.profile.user_profile' )) }}">
+                                            </figure>
+                                            <span>username</span>
+                                        </div>
+                                        <div class="cell shrink like"><i class="far fa-thumbs-up"></i><span>415 likes</span></div>
+                                        <div class="cell shrink comment"><i class="far fa-comments"></i><span>500 comments</span></div>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="cell grid-x medium-12 large-6">
+                                <div class="cell small-12 medium-5">
+                                    <figure>
+                                        <img src="{{ asset(config('images.home.share.home_share_01' )) }}">
+                                    </figure>
+                                </div>
+                                <div class="cell grid-x small-12 medium-7">
+                                    <h4 class="cell">Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget nibh arcu.</h4>
+                                    <div class="cell grid-x align-self-bottom">
+                                        <div class="cell auto profile">
+                                            <figure class="display-profile">
+                                                <img src="{{ asset(config('images.home.profile.user_profile' )) }}">
+                                            </figure>
+                                            <span>username</span>
+                                        </div>
+                                        <div class="cell shrink like"><i class="far fa-thumbs-up"></i><span>415 likes</span></div>
+                                        <div class="cell shrink comment"><i class="far fa-comments"></i><span>500 comments</span></div>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="cell grid-x medium-12 large-6">
+                                <div class="cell small-12 medium-5">
+                                    <figure>
+                                        <img src="{{ asset(config('images.home.share.home_share_01' )) }}">
+                                    </figure>
+                                </div>
+                                <div class="cell grid-x small-12 medium-7">
+                                    <h4 class="cell">Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget nibh arcu.</h4>
+                                    <div class="cell grid-x align-self-bottom">
+                                        <div class="cell auto profile">
+                                            <figure class="display-profile">
+                                                <img src="{{ asset(config('images.home.profile.user_profile' )) }}">
+                                            </figure>
+                                            <span>username</span>
+                                        </div>
+                                        <div class="cell shrink like"><i class="far fa-thumbs-up"></i><span>415 likes</span></div>
+                                        <div class="cell shrink comment"><i class="far fa-comments"></i><span>500 comments</span></div>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="cell grid-x medium-12 large-6">
+                                <div class="cell small-12 medium-5">
+                                    <figure>
+                                        <img src="{{ asset(config('images.home.share.home_share_01' )) }}">
+                                    </figure>
+                                </div>
+                                <div class="cell grid-x small-12 medium-7">
+                                    <h4 class="cell">Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget nibh arcu.</h4>
+                                    <div class="cell grid-x align-self-bottom">
+                                        <div class="cell auto profile">
+                                            <figure class="display-profile">
+                                                <img src="{{ asset(config('images.home.profile.user_profile' )) }}">
+                                            </figure>
+                                            <span>username</span>
+                                        </div>
+                                        <div class="cell shrink like"><i class="far fa-thumbs-up"></i><span>415 likes</span></div>
+                                        <div class="cell shrink comment"><i class="far fa-comments"></i><span>500 comments</span></div>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="cell grid-x medium-12 large-6">
+                                <div class="cell small-12 medium-5">
+                                    <figure>
+                                        <img src="{{ asset(config('images.home.share.home_share_01' )) }}">
+                                    </figure>
+                                </div>
+                                <div class="cell grid-x small-12 medium-7">
+                                    <h4 class="cell">Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget nibh arcu.</h4>
+                                    <div class="cell grid-x align-self-bottom">
+                                        <div class="cell auto profile">
+                                            <figure class="display-profile">
+                                                <img src="{{ asset(config('images.home.profile.user_profile' )) }}">
+                                            </figure>
+                                            <span>username</span>
+                                        </div>
+                                        <div class="cell shrink like"><i class="far fa-thumbs-up"></i><span>415 likes</span></div>
+                                        <div class="cell shrink comment"><i class="far fa-comments"></i><span>500 comments</span></div>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="cell grid-x medium-12 large-6">
+                                <div class="cell small-12 medium-5">
+                                    <figure>
+                                        <img src="{{ asset(config('images.home.share.home_share_01' )) }}">
+                                    </figure>
+                                </div>
+                                <div class="cell grid-x small-12 medium-7">
+                                    <h4 class="cell">Title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget nibh arcu.</h4>
+                                    <div class="cell grid-x align-self-bottom">
+                                        <div class="cell auto profile">
+                                            <figure class="display-profile">
+                                                <img src="{{ asset(config('images.home.profile.user_profile' )) }}">
+                                            </figure>
+                                            <span>username</span>
+                                        </div>
+                                        <div class="cell shrink like"><i class="far fa-thumbs-up"></i><span>415 likes</span></div>
+                                        <div class="cell shrink comment"><i class="far fa-comments"></i><span>500 comments</span></div>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    <div class="tabs-panel" id="panel2c">
+                        ดด
+                    </div>
+                </div>
+            </div>
+        </div>
+   </section>
 @endsection
