9842852ecb5c4560e765d7baeb54726cdd9931b5
EaRNG <e_a_r_n_g@hotmail.com>
2019-05-22 14:22:45 +0700

    update give section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -246,7 +246,49 @@
             </div>
         </div>
         <div class="grid-x content padding-content">
-            None
+            <div class="cell medium-3">
+            <ul class="vertical tabs" data-tabs id="example-tabs">
+                <li class="">Categories</li>
+                <li class="tabs-title is-active"><a href="#panel1v" aria-selected="true">Food non-perishable</a></li>
+                <li class="tabs-title"><a href="#panel2v">Food perishable</a></li>
+                <li class="tabs-title"><a href="#panel3v">School supply</a></li>
+                <li class="tabs-title"><a href="#panel4v">Housing</a></li>
+                <li class="tabs-title"><a href="#panel5v">Medical supplies</a></li>
+                <li class="tabs-title"><a href="#panel6v">Consulting</a></li>
+                <li class="tabs-title"><a href="#panel6v">Transportation</a></li>
+                <li class="tabs-title"><a href="#panel6v">Service</a></li>
+                <li class="tabs-title"><a href="#panel6v">Volunteer</a></li>
+                <li class="tabs-title"><a href="#panel6v">Other</a></li>
+            </ul>
+            </div>
+            <div class="cell medium-9">
+            <div class="tabs-content" data-tabs-content="example-tabs">
+                <div class="tabs-panel is-active" id="panel1v">
+                <p>One</p>
+                <p>Check me out! I'm a super cool Tab panel with text content!</p>
+                </div>
+                <div class="tabs-panel" id="panel2v">
+                <p>Two</p>
+                <img class="thumbnail" src="assets/img/generic/rectangle-7.jpg">
+                </div>
+                <div class="tabs-panel" id="panel3v">
+                <p>Three</p>
+                <p>Check me out! I'm a super cool Tab panel with text content!</p>
+                </div>
+                <div class="tabs-panel" id="panel4v">
+                <p>Four</p>
+                <img class="thumbnail" src="assets/img/generic/rectangle-2.jpg">
+                </div>
+                <div class="tabs-panel" id="panel5v">
+                <p>Five</p>
+                <p>Check me out! I'm a super cool Tab panel with text content!</p>
+                </div>
+                <div class="tabs-panel" id="panel6v">
+                <p>Six</p>
+                <img class="thumbnail" src="assets/img/generic/rectangle-8.jpg">
+                </div>
+            </div>
+            </div>
         </div>
    </section>
 
