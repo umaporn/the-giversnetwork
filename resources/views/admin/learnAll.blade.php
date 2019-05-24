c8a418697242ceba0a4c3c6ad694ed49853c5af4
phurichaya sirisansaneeyakul <phurichaya@phurichayas-MacBook-Pro.local>
2019-05-24 10:23:57 +0700

    update all learn page

--- resources/views/admin/learnAll.blade.php
+++ resources/views/admin/learnAll.blade.php
@@ -30,7 +30,93 @@
                 <div class="cell small-12 large-8 xxlarge-9">
                     <article class="admin-content">
                         <div class="grid-x">
-                            <div class="cell small-12">ddsdsds</div>
+                            <div class="cell small-12">
+                                <div class="grid-x admin-form-space admin-search">
+                                    <h2 class="cell shrink admin-head">All Learn</h2>
+                                    <div class="cell auto grid-x align-middle">
+                                        <div class="cell line auto"></div>
+                                        <div class="cell shrink">
+                                            <span class="outline-dot float-right"><span class="dot"></span></span>
+                                        </div>
+                                        <div class="margin-left-1">
+                                            <div class="input-group input-search">
+                                                <input class="input-group-field form-fill" type="text">
+                                                <div class="input-group-button">
+                                                    <input type="button" class="button btn-blue" value="Search">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cell small-12">
+                                <section class="table-scroll table-admin">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Title</th>
+                                                <th>Views</th>
+                                                <th>Publish</th>
+                                                <th>URL</th>
+                                                <th>Edit</th>
+                                                <th>Delete</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>00019</td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor erat nec augue sollicitudin, eu vulputate enim vestibulum. In hac habitasse platea dictumst. Mauris tincidunt metus turpis, eget interdum metus </td>
+                                                <td>2.1k</td>
+                                                <td><i class="far fa-check-square"></i></td>
+                                                <td><a href="#"><i class="fas fa-link"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-pen"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
+                                            </tr>
+                                            <tr>
+                                                <td>00019</td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                                                <td>2.1k</td>
+                                                <td><i class="far fa-check-square"></i></td>
+                                                <td><a href="#"><i class="fas fa-link"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-pen"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
+                                            </tr>
+                                            <tr>
+                                                <td>00019</td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                                                <td>2.1k</td>
+                                                <td><i class="far fa-check-square"></i></td>
+                                                <td><a href="#"><i class="fas fa-link"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-pen"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
+                                            </tr>
+                                            <tr>
+                                                <td>00019</td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                                                <td>2.1k</td>
+                                                <td><i class="far fa-check-square"></i></td>
+                                                <td><a href="#"><i class="fas fa-link"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-pen"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </section>
+                                <nav aria-label="Pagination">
+                                    <ul class="pagination text-center">
+                                        <li class="pagination-previous disabled">Previous</li>
+                                        <li class="current"><span class="show-for-sr">You're on page</span> 1</li>
+                                        <li><a href="#" aria-label="Page 2">2</a></li>
+                                        <li><a href="#" aria-label="Page 3">3</a></li>
+                                        <li><a href="#" aria-label="Page 4">4</a></li>
+                                        <li class="ellipsis"></li>
+                                        <li><a href="#" aria-label="Page 12">12</a></li>
+                                        <li><a href="#" aria-label="Page 13">13</a></li>
+                                        <li class="pagination-next"><a href="#" aria-label="Next page">Next</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
                         </div>
                     </article>
                 </div>
