892591e4efa27a8db5cfffea1ec27334f0c4b2c5
Umaporn_do <umaporn_do@dtgsiam.com>
2019-05-27 17:58:42 +0700

    Updated news url in learn detail page.

--- resources/views/learn/index.blade.php
+++ resources/views/learn/index.blade.php
@@ -68,7 +68,7 @@
                     <div class="grid-x align-middle">
                         <h2 class="cell auto topic-dark">@lang('events.page_link.index')</h2>
                         <div class="cell shrink view-all">
-                            <a href="#">
+                            <a href="{{ route('events.index') }}">
                                 <span>@lang('button.view_all')</span>
                                 <i class="fas fa-caret-right"></i> <i class="fas fa-caret-right"></i>
                             </a>
@@ -79,15 +79,17 @@
                             <article class="cell">
                                 <div class="grid-x grid-margin-x large-margin-collapse">
                                     <div class="cell small-12 medium-6 large-12">
-                                        <figure class="cover">
-                                            <img src="{{ $events_item['image_path'] }}" alt="{{ $events_item['title'] }}">
-                                            <figcaption>
-                                                <time datetime="{{ $events_item['event_date'] }}">{{ $events_item['event_date'] }}</time>
-                                            </figcaption>
-                                        </figure>
+                                        <a href="{{ route('events.detail', ['events' => $events_item['id']]) }}">
+                                            <figure class="cover">
+                                                <img src="{{ $events_item['image_path'] }}" alt="{{ $events_item['title'] }}">
+                                                <figcaption>
+                                                    <time datetime="{{ $events_item['event_date'] }}">{{ $events_item['event_date'] }}</time>
+                                                </figcaption>
+                                            </figure>
+                                        </a>
                                     </div>
                                     <div class="cell small-12 medium-6 large-12">
-                                        <a href="#">
+                                        <a href="{{ route('events.detail', ['events' => $events_item['id']]) }}">
                                             <h3>{{ $events_item['title'] }}</h3>
                                         </a>
                                         <div class="profile">
@@ -107,7 +109,7 @@
                     <div class="grid-x align-middle">
                         <h2 class="cell auto topic-dark">@lang('news.page_link.index')</h2>
                         <div class="cell shrink view-all">
-                            <a href="#">
+                            <a href="{{ route('news.index') }}">
                                 <span>@lang('button.view_all')</span>
                                 <i class="fas fa-caret-right"></i> <i class="fas fa-caret-right"></i>
                             </a>
@@ -115,23 +117,26 @@
                     </div>
                     <div class="grid-x grid-margin-x grid-margin-y">
                         @foreach( $data['news'] as $news_items )
-                        <article class="cell">
-                            <div class="grid-x grid-margin-x large-margin-collapse">
-                                <div class="cell small-12 medium-6 large-12">
-                                    <figure>
-                                        <img src="{{ $news_items['images'] }}" alt="{{ $news_items['title'] }}">
-                                    </figure>
+                            <article class="cell">
+                                <div class="grid-x grid-margin-x large-margin-collapse">
+                                    <div class="cell small-12 medium-6 large-12">
+                                        <figure>
+                                            <a href="{{ route('news.detail', [ 'news' => $news_items['id'] ]) }}">
+                                                <img src="{{ $news_items['image_path'] }}" alt="{{ $news_items['title'] }}">
+                                            </a>
+                                        </figure>
+                                    </div>
+                                    <div class="cell small-12 medium-6 large-12">
+                                        <a href="{{ route('news.detail', [ 'news' => $news_items['id'] ]) }}">
+                                            <h3>
+                                                {{ $news_items['title'] }}
+                                            </h3>
+                                        </a>
+                                        <time datetime="2019-04-29">
+                                            <i class="far fa-calendar-alt"></i>{{ $news_items['public_date'] }}</time>
+                                    </div>
                                 </div>
-                                <div class="cell small-12 medium-6 large-12">
-                                    <a href="#">
-                                        <h3>
-                                            {{ $news_items['title'] }}
-                                        </h3>
-                                    </a>
-                                    <time datetime="2019-04-29"><i class="far fa-calendar-alt"></i>{{ $news_items['public_date'] }}</time>
-                                </div>
-                            </div>
-                        </article>
+                            </article>
                         @endforeach
                     </div>
                 </section>
