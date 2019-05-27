2ad2355406bc8701c3808b925e0a4b077d439a9d
Jiranuch <jiranuch_ch@dtgo.com>
2019-05-27 11:26:28 +0700

    Fix indent

--- public/js/app.js
+++ public/js/app.js
@@ -18768,7 +18768,7 @@ window.Swiper = __webpack_require__("./node_modules/swiper/dist/js/swiper.js");
 /***/ "./resources/assets/sass/app.scss":
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n@import \"components/animation\";\n^\n      File to import not found or unreadable: /Applications/MAMP/htdocs/the-giversnetwork/resources/assets/sass/components/_animation.scss.\n      in /Applications/MAMP/htdocs/the-giversnetwork/resources/assets/sass/app.scss (line 22, column 1)\n    at runLoaders (/Applications/MAMP/htdocs/the-giversnetwork/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Applications/MAMP/htdocs/the-giversnetwork/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Applications/MAMP/htdocs/the-giversnetwork/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Applications/MAMP/htdocs/the-giversnetwork/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Applications/MAMP/htdocs/the-giversnetwork/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/Applications/MAMP/htdocs/the-giversnetwork/node_modules/neo-async/async.js:8077:18)\n    at options.error (/Applications/MAMP/htdocs/the-giversnetwork/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ }),
 
