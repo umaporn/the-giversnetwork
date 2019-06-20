8387433d5575fbe45225ba4e025b7cdcf1b5a979
umaporn_do <umaporn_do@dtgo.com>
2019-06-20 08:28:43 +0700

    Added condition for banner when banner have only one image.

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,4 +1,23 @@
-<div class="swiper-container">
+@if($data['banner']->total() !== 1)
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            @foreach( $data['banner'] as $home_banner )
+                <div class="swiper-slide">
+                    <a href="{{ $home_banner['link'] }}" target="_blank">
+                        <img src="{{ $home_banner['image_path'] ? $home_banner['image_path'] : config('images.placeholder.1900x500') }}" alt="{{$home_banner['title']}}">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        <div class="swiper-button-next">
+            <i class="fas fa-chevron-right fa-3x"></i>
+        </div>
+        <div class="swiper-button-prev">
+            <i class="fas fa-chevron-left fa-3x"></i>
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
+    @else
     <div class="swiper-wrapper">
         @foreach( $data['banner'] as $home_banner )
             <div class="swiper-slide">
@@ -8,11 +27,4 @@
             </div>
         @endforeach
     </div>
-    <div class="swiper-button-next">
-        <i class="fas fa-chevron-right fa-3x"></i>
-    </div>
-    <div class="swiper-button-prev">
-        <i class="fas fa-chevron-left fa-3x"></i>
-    </div>
-    <div class="swiper-pagination"></div>
-</div>
+@endif
