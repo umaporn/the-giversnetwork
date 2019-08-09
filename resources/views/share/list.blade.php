523c8d4711e8c7aa313b6cd0129bf4b411e56ff2
umaporn_do <umaporn_do@dtgo.com>
2019-08-09 13:26:53 +0700

    Fixed share image is empty.

--- resources/views/share/list.blade.php
+++ resources/views/share/list.blade.php
@@ -4,7 +4,7 @@
             <div class="cell small-12 medium-6">
                 <figure class="share-image">
                     <a href="{{ route('share.detail', ['share' => $share_item['id']]) }}">
-                        <img src="{{ $share_item->shareImage[0]['image_path'] ? $share_item->shareImage[0]['image_path'] : config('images.placeholder.300x180') }}"
+                        <img src="{{ empty( $share_item->shareImage ) ? $share_item->shareImage[0]['image_path'] : config('images.placeholder.700x400') }}"
                              alt="{{ $share_item['title'] }}">
                     </a>
                 </figure>
